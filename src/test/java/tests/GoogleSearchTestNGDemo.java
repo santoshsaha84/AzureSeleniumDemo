c9c614a738a5aae63558d5b53a9322723b7cb008
santoshsaha84 <santosh_saha84@yahoo.co.in>
2020-08-17 14:03:40 +0530

    Headless chrome2

--- src/test/java/tests/GoogleSearchTestNGDemo.java
+++ src/test/java/tests/GoogleSearchTestNGDemo.java
@@ -3,6 +3,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -34,8 +35,11 @@ public class GoogleSearchTestNGDemo {
 	
 	@Test
 	public void googleSearch()
-	{		
-		driver=new ChromeDriver();
+	{	
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1200x600");
+		driver=new ChromeDriver(options);
 		driver.get("https://google.com");		
 		GoogleSearchPageObject googlePage=new GoogleSearchPageObject(driver);
 		googlePage.setTextInSearchBox("A B C D");
