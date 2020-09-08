ee1de206d12e8cfb306c6768165c68c0ddb134b2
santoshsaha84 <santosh_saha84@yahoo.co.in>
2020-09-08 17:25:15 +0530

    changing chrome binary location

--- src/test/java/tests/GoogleSearchTestNGDemo.java
+++ src/test/java/tests/GoogleSearchTestNGDemo.java
@@ -4,6 +4,7 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,12 +14,15 @@ import pages.GoogleSearchPageObject;
 public class GoogleSearchTestNGDemo {
 	
 	 WebDriver driver=null;
+	 DesiredCapabilities capability;
 	
 	@BeforeTest
 	public void Setup()
 	{
 		String projectPath=System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", projectPath+"\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		capability = new DesiredCapabilities();
+		capability.setCapability("binary", "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
 		
 	}
 	
@@ -37,6 +41,7 @@ public class GoogleSearchTestNGDemo {
 	public void googleSearch()
 	{	
 		ChromeOptions options = new ChromeOptions();
+		options.merge(capability);
         options.addArguments("headless");
         options.addArguments("window-size=1200x600");
 		driver=new ChromeDriver(options);
