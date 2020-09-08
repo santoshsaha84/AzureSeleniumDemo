d6f14124e90063e0823a01950ccdf77b6e616847
santoshsaha84 <santosh_saha84@yahoo.co.in>
2020-09-08 13:58:37 +0530

    commenting other lines

--- src/test/java/demo/PriorityTest.java
+++ src/test/java/demo/PriorityTest.java
@@ -1,24 +1,24 @@
-package demo;
-
-import org.testng.annotations.Test;
-
-public class PriorityTest {
-	@Test(priority=3)
-	public void Test1()
-	{
-		System.out.println("Inside Test1");
-	}
-	
-	@Test(priority=1)
-	public void Test2()
-	{
-		System.out.println("Inside Test2");
-	}
-	
-	@Test(priority=2)
-	public void Test3()
-	{
-		System.out.println("Inside Test3");
-	}
-
-}
+//package demo;
+//
+//import org.testng.annotations.Test;
+//
+//public class PriorityTest {
+//	@Test(priority=3)
+//	public void Test1()
+//	{
+//		System.out.println("Inside Test1");
+//	}
+//	
+//	@Test(priority=1)
+//	public void Test2()
+//	{
+//		System.out.println("Inside Test2");
+//	}
+//	
+//	@Test(priority=2)
+//	public void Test3()
+//	{
+//		System.out.println("Inside Test3");
+//	}
+//
+//}
